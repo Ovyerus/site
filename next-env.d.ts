6774e6d77ba3eb89e7b1b8677080afa9e6670352
Ovyerus <ovy@ovyerus.com>
2020-02-03 22:38:02 +1100

    feat: index page

---
+++ next-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+
+declare module '*.webp' {
+  const data: string;
+  export default data;
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document_, { Html, Head, Main, NextScript } from 'next/document';
+
+class Document extends Document_ {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+
+          <link
+            rel="stylesheet"
+            href="/dark.css"
+            media="(prefers-color-scheme: dark)"
+          />
+          <link
+            rel="stylesheet"
+            href="/light.css"
+            media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+export default Document;
