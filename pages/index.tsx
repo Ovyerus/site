0007909c2f944c510feca4062bd45bcfad176aab
Ovyerus <iamovyerus@gmail.com>
2020-08-06 14:31:07 +1000

    create

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,16 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { NextPage } from 'next';
-import Link_, { LinkProps } from 'next/link';
 import { NextSeo } from 'next-seo';
-import React from 'react';
 
 import { Layout, Title, Paragraph, Anchor } from '../components';
-
-const Link: React.FC<LinkProps> = ({ children, ...props }) => (
-  <Link_ {...props} passHref>
-    <Anchor>{children}</Anchor>
-  </Link_>
-);
 
 const IndexPage: NextPage = () => (
   <Layout mainPage>
     <NextSeo title="Ovyerus" titleTemplate="%s" />
 
     <Title>Ovyerus</Title>
-    <Paragraph>
-      Multifaceted creative
-      <br />- <Link href="/design">design</Link>,{' '}
-      {/* <Link href="/music">music</Link> */} music, and{' '}
-      <Link href="/code">code</Link>.
-    </Paragraph>
+    <Paragraph>Multifaceted creative.</Paragraph>
 
     <Paragraph
       css={css`
