3fe53ca90b7b04f76b53750f65843a9370d7c13e
Ovyerus <ovy@ovyerus.com>
2021-02-11 12:23:38 +1100

    chore: upgrade dependencies

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,4 @@
-/** @jsx jsx */
-import { css, jsx } from '@emotion/core';
+import { css, jsx } from '@emotion/react';
 import Icon from '@mdi/react';
 import React from 'react';
 import dribbble from 'simple-icons/icons/dribbble';
