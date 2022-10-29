4bb59a7c69686994f9aeb27976607edfe3a8c1fb
Michael Mitchell <ovy@ovyerus.com>
2022-10-29 14:03:11 +1100

    feat: compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
+import { defineConfig } from "astro/config";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch()],
+  integrations: [tailwind(), prefetch(), compress()],
   site: "https://ovyerus.com",
 });
