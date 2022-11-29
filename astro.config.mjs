dc23c2299750a070d8171d39acb0f071cd9050e2
Ovyerus <ovy@ovyerus.com>
2022-11-29 11:52:24 +1100

    feat: styles & layout for articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,30 @@
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
+import a11yEmoji from "@fec/remark-a11y-emoji";
+import remarkFigureCaption from "@microflash/remark-figure-caption";
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+
+import dolch from "./src/dolch.theme.json";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), prefetch(), compress()],
   site: "https://ovyerus.com",
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [remarkFigureCaption, a11yEmoji],
+    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+    shikiConfig: {
+      wrap: true,
+      theme: {
+        ...dolch,
+        bg: "#101416",
+        fg: "#b0b4b7",
+        type: "dark",
+      },
+    },
+  },
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,84 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      mono: ["Iosevka", ...defaultTheme.fontFamily.mono],
     },
-    extend: {},
+    extend: {
+      typography: ({ theme }) => ({
+        invert: {
+          css: {
+            "--tw-prose-invert-bullets": "#0ff",
+            "--tw-prose-invert-counters": "#0ff",
+            "--tw-prose-invert-quote-borders": "transparent",
+            hr: {
+              borderTopWidth: "0",
+              height: "1px",
+              background: "linear-gradient(to right, #f0f, #0ff)",
+            },
+            "a:hover": {
+              background: "linear-gradient(to bottom right, #f0f, #0ff)",
+              backgroundClip: "text",
+              color: "transparent",
+            },
+            pre: {
+              fontStyle: "normal",
+            },
+            code: {
+              position: "relative",
+              background: "linear-gradient(to right, #f0f, #0ff)",
+              backgroundClip: "text",
+              color: "transparent",
+              paddingTop: theme("padding[1]"),
+              paddingRight: theme("padding[1.5]"),
+              paddingBottom: theme("padding[1]"),
+              paddingLeft: theme("padding[1.5]"),
+              fontSize: theme("fontSize.base"),
+            },
+            "code::before": {
+              content: "''",
+              display: "block",
+              position: "absolute",
+              inset: "0",
+              borderRadius: theme("borderRadius.md"),
+              backgroundColor: "#101416",
+              zIndex: "-1",
+            },
+            "code::after": {
+              content: "normal",
+            },
+            blockquote: {
+              position: "relative",
+            },
+            "blockquote::before": {
+              content: "''",
+              display: "block",
+              position: "absolute",
+              background: "linear-gradient(to bottom, #f0f, #0ff)",
+              width: "0.25rem",
+              left: "0",
+              top: "0",
+              bottom: "0",
+            },
+            figure: {
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            },
+            figcaption: {
+              fontStyle: "italic",
+              color: "white",
+              opacity: "0.75",
+            },
+            "figure img": {
+              maxHeight: "500px",
+              width: "auto",
+              height: "100%",
+              borderRadius: theme("borderRadius.md"),
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
