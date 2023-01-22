a25217f96afc65768d47321893ba484b818701dd
Ovyerus <ovy@ovyerus.com>
2023-01-22 15:50:57 +1100

    chore: add prettier-plugin-tailwindcss

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,10 @@
 /** @type import('prettier').Config */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-astro")],
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  pluginSearchDirs: false,
   proseWrap: "always",
   overrides: [
     {
