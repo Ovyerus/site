4a80f9cab67fb45aafb3b13e64af7cc678f7f51c
Chloe <chloe@sapphic.moe>
2025-01-26 05:40:45 +0500

    chore(flake): use flakeExposed to build packages for all systems

--- flake.nix
+++ flake.nix
@@ -4,27 +4,24 @@
   };
 
   outputs = {nixpkgs, ...}: let
-    forSystems = fn:
-      nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ] (system: fn nixpkgs.legacyPackages.${system});
-    defaultForSystems = fn: forSystems (pkgs: {default = fn pkgs;});
-  in {
-    devShells = defaultForSystems (
-      pkgs:
-        with pkgs;
-          mkShell {
-            nativeBuildInputs = [nodejs_22 yarn vips pkg-config flyctl];
-            shellHook = ''
-              export LD_LIBRARY_PATH="${lib.makeLibraryPath [vips]}:$LD_LIBRARY_PATH"
-            '';
-          }
-    );
+    forAllSystems =
+      function:
+      nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+        system: function nixpkgs.legacyPackages.${system}
+      );
+    in {
+      devShells = forAllSystems (
+        pkgs:
+          with pkgs;
+            mkShell {
+              nativeBuildInputs = [nodejs_22 yarn vips pkg-config flyctl];
+              shellHook = ''
+                export LD_LIBRARY_PATH="${lib.makeLibraryPath [vips]}:$LD_LIBRARY_PATH"
+              '';
+            }
+      );
 
-    packages = forSystems (pkgs: let
+    packages = forAllSystems (pkgs: let
       site = pkgs.callPackage ./site.nix {};
       container = pkgs.callPackage ./container.nix {inherit site;};
     in {
